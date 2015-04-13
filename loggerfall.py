4dfadeac022834347de93a2812c4bfa7857de054
Michael <michael_gugino@cable.comcast.com>
2015-04-13 17:56:26 -0400

    added function to read from redis cache

--- loggerfall.py
+++ loggerfall.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-#
-# Copyright 2009 Facebook
+# Modified work by Michael Gugino
+# Original work Copyright 2009 Facebook
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -28,9 +28,13 @@ import os.path
 import uuid
 from tornado.httpserver import HTTPServer
 from tornado.options import define, options
+import redis
+import ast
+
 
 #define("port", default=8888, help="run on the given port", type=int)
-
+pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+redcon = redis.Redis(connection_pool=pool)
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -73,19 +77,21 @@ class ChatSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         hostchannel = str(self.get_argument("HOST", default='none', strip=False))
         appchannel = str(self.get_argument("APP", default='none', strip=False))
+        hostappchan = hostchannel + '::' + appchannel
         if (hostchannel == 'none' or appchannel == 'none'):
           return none
-        self.subscribe = hostchannel+'::'+appchannel
-        if str(hostchannel+'::'+appchannel) in ChatSocketHandler.channels:
-          ChatSocketHandler.channels[hostchannel+'::'+appchannel].add(self)
+        self.subscribe = hostappchan
+        if str(hostappchan) in ChatSocketHandler.channels:
+          ChatSocketHandler.channels[hostappchan].add(self)
         else:
-          ChatSocketHandler.channels[hostchannel+'::'+appchannel] = set()
-          ChatSocketHandler.channels[hostchannel+'::'+appchannel].add(self)
-        #ChatSocketHandler.waiters.add(self)
+          ChatSocketHandler.channels[hostappchan] = set()
+          ChatSocketHandler.channels[hostappchan].add(self)
     def on_close(self):
-        #ChatSocketHandler.waiters.remove(self)
-        ChatSocketHandler.channels[hostchannel+'::'+appchannel].remove(self)
-
+        hostchannel = str(self.get_argument("HOST", default='none', strip=False))
+        appchannel = str(self.get_argument("APP", default='none', strip=False))
+        hostappchan = hostchannel + '::' + appchannel
+        ChatSocketHandler.channels[hostappchan].remove(self)
+ 
     @classmethod
     def update_cache(cls,channel,chat):
         #cls.cache.append(chat)
@@ -104,26 +110,51 @@ class ChatSocketHandler(tornado.websocket.WebSocketHandler):
               print "waiters", waiter
               #logging.info("sending message to %d waiters", len())
               try:
+                  print "send_updates: ", type(chat)
                   waiter.write_message(chat)
               except:
                   logging.error("Error sending message", exc_info=True)
                   cls.channels[channel].remove(waiter)
+    @classmethod
+    def send_updates2(cls, channel, chat):
+      logging.info("sending message to %d waiters", len(cls.channels[channel]))
+      for waiter in cls.channels[channel]:
+        print channel
+        print "chat: ", chat
+        print "waiters", waiter
+        try:
+          for msg in redcon.lrange(channel,-200,-1):
+            #print "msg: ", type(msg)
+            waiter.write_message(ast.literal_eval(msg))
+          #waiter.write_message(chat)
+        except:
+          logging.error("Error sending message", exc_info=True)
+          cls.channels[channel].remove(waiter)
 
     def on_message(self, message):
         pass
-       # logging.info("got message %r", message)
-       # parsed = tornado.escape.json_decode(message)
-       # chat = {
-       #     "id": str(uuid.uuid4()),
-       #     "body": parsed["body"],
-       #     }
-       # chat["html"] = tornado.escape.to_basestring(
-       #     self.render_string("message.html", message=chat))
 
-#        ChatSocketHandler.update_cache(chat)
-#        ChatSocketHandler.send_updates(chat)
-def t1():
-  print 't1'
+
+def push_to_redis(app,message):
+  parsed = tornado.escape.json_decode(message)
+  chat = {
+    "id": str(uuid.uuid4()),
+    "body": parsed["body"],
+    }
+  
+  if ChatSocketHandler.channels:
+    removals = list()
+    for channel in ChatSocketHandler.channels:
+      if len(ChatSocketHandler.channels[channel]) == 0:
+        removals.append(ChatSocketHandler.channels[channel])
+      else:
+        chat["id"] = str(uuid.uuid4())
+        chat["html"] = """<div class="message" id="#m%s">%s : %s</div>""" % (chat['id'], channel, str(parsed['body'] + chat["id"]))
+        print "push_to_redis", chat
+        redcon.rpush(channel,chat)
+        ChatSocketHandler.send_updates2(channel,chat)
+    for chan in removals:
+      del ChatSocketHandler.channels[channel]
 
 def main_on_message(app,message):
         parsed = tornado.escape.json_decode(message)
@@ -143,6 +174,7 @@ def main_on_message(app,message):
               #this is where we'll pull data from redis
               print "channel: ",channel
               chat["html"] = """<div class="message" id="0">%s : %s</div>""" % (channel, parsed['body'])
+              print "main_on_message: ", chat
               ChatSocketHandler.update_cache(channel,chat)
               #should change this method to only send new info
               ChatSocketHandler.send_updates(channel,chat)
@@ -165,7 +197,9 @@ def main(**kwargs):
       #print app.handlers[0][1][4].handler_class.cache
       #print ChatSocketHandler.cache
       main_on_message(app,u'{"body":"test","_xsrf":"2|7f1e46b3|f4ea7f3d2aeef31b4aabb4af00886db3|1426865952"}')
-    pcall = tornado.ioloop.PeriodicCallback(t2, 2000)
+    def t3():
+      push_to_redis(app,u'{"body":"test","_xsrf":"2|7f1e46b3|f4ea7f3d2aeef31b4aabb4af00886db3|1426865952"}')
+    pcall = tornado.ioloop.PeriodicCallback(t3, 2000)
     pcall.start()
     goer.start()
 
